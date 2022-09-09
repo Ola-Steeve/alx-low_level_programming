79f20eb9382abbbb9618c59ffb5a880aa0eb66d7
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-09 08:45:21 -0700

    bug fixes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
 int main()
 {
-	printf("The size of int is:\n",sizeof(int));
-	printf("The size of float is:\n",sizeof(float));
-	printf("The size of char is :\n",sizeof(char));
-	printf("The siz of double is:\n",sizeof(double));
+	printf("The size of int is:%d byte(s)\n", sizeof(int));
+	printf("The size of float is:%d byte(s)\n", sizeof(float));
+	printf("The size of char is :%d byte(s)\n", sizeof(char));
+	printf("The siz of double is:%d byte(s)\n", sizeof(double));
 	return(0);
 }
