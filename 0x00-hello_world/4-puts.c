f903783155605a31896884278aeec95d319e7a8a
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-09 06:59:20 -0700

    introductions to c programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+{
+	char olastr[]= "Programming is like building a multilingual puzzle\n";
+	
+
+	puts(olastr);
+
+	return (0);
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("The size of int is:\n",sizeof(int));
+	printf("The size of float is:\n",sizeof(float));
+	printf("The size of char is :\n",sizeof(char));
+	printf("The siz of double is:\n",sizeof(double));
+	return(0);
+}
