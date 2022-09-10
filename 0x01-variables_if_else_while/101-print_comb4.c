cbff4129342aff849f44090c8ec7fb51fe77906a
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-09 18:27:06 -0700

    new fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -21,13 +21,13 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i !=7 || j != 8 || k != 9)
+					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-			}	
+			}
 		}
 	}
 	putchar('\n');
