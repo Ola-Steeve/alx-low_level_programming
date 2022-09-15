3097b53251e1de0f407b2c216a22cdd47235c01e
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-14 17:16:05 -0700

    mandatory tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,21 +2,19 @@
 /**
  * print_times_table - times table fnction
  * @n: integer to return
- *
  */
 void print_times_table(int n)
 {
 	int i;
 	int j;
 
-	if (n <=15 && n >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				int prod = j * i;
-
 				if (j == 0)
 				{
 					_putchar('0');
