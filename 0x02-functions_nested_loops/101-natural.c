258d72c0364a2f759c751f80a82e613fde014e3f
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-14 17:35:03 -0700

    mandatory tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,19 +2,16 @@
 /**
  *main - Entry point
  *
- *Return: Always 0(Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int sum = 0;
-
-	for (n = 0; n < 1024; n++)
-	{
-		if ((n % 3) == 0 || (n % 5) == 0)
-		{
-			sum += n;
-		}
-	}
-	printf("%d\n", sum);
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+sum += n;
+}
+printf("%d\n", sum);
+return (0);
 }
