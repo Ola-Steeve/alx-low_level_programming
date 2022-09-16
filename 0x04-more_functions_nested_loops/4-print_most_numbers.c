cc14098da775f5926b081a07241801e7f734ab2e
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2022-09-15 17:48:26 -0700

    new fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,10 @@ int number;
 
 for (number = '0'; number <= '9'; number++)
 {
-if (number == '2' || number == '4')
+if (number != '2' && number != '4')
 {
-;
+_putchar(i);
 }
-else
-{
-_putchar(number);
 }
 _putchar('\n');
 }
